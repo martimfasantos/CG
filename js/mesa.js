d43bffcf432b91c873d805a961abf68029cbf13a
martimfasantos <martimfasantos@gmail.com>
2022-05-11 19:19:56 +0100

    first changes

--- js/mesa.js
+++ js/mesa.js
@@ -1,10 +1,44 @@
-/*global THREE, requestAnimationFrame, console*/
+/* global THREE */
+
+/* TODO 
+    ERROS A CORRIGIR
+    - render chamado multiplas vezes - evitar chamar tantas vezes o render
+    (desenhar table, perna, perna, perna, perna then render)
+*/
 
 var camera, scene, renderer;
-
 var geometry, material, mesh;
-
 var ball;
+
+
+function createTable(x, y, z) {
+    'use strict';
+
+    var table = new THREE.Object3D();
+
+    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
+    
+    addTableTop(table, 0, 0, 0);
+    addTableLeg(table, -25, -1, -8);
+    addTableLeg(table, -25, -1, 8);
+    addTableLeg(table, 25, -1, 8);
+    addTableLeg(table, 25, -1, -8);
+    
+    scene.add(table);
+    
+    table.position.x = x;
+    table.position.y = y;
+    table.position.z = z;
+}
+
+function addTableTop(obj, x, y, z) {
+    'use strict';
+
+    geometry = new THREE.CubeGeometry(60, 2, 20);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
 
 function addTableLeg(obj, x, y, z) {
     'use strict';
@@ -15,21 +49,13 @@ function addTableLeg(obj, x, y, z) {
     obj.add(mesh);
 }
 
-function addTableTop(obj, x, y, z) {
-    'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
-}
-
 function createBall(x, y, z) {
     'use strict';
 
     ball = new THREE.Object3D();
     ball.userData = { jumping: true, step: 0 };
 
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    material = new THREE.MeshBasicMaterial({ color: 0x40E0D0, wireframe: true });
     geometry = new THREE.SphereGeometry(4, 10, 10);
     mesh = new THREE.Mesh(geometry, material);
 
@@ -40,48 +66,19 @@ function createBall(x, y, z) {
 }
 
 
-function createTable(x, y, z) {
-    'use strict';
-
-    var table = new THREE.Object3D();
-
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
-
-    scene.add(table);
-
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
-}
-
-function createScene() {
-    'use strict';
-
-    scene = new THREE.Scene();
-
-
-    scene.add(new THREE.AxisHelper(10));
-
-    createTable(0, 8, 0);
-    createBall(0, 0, 15);
-}
-
 function createCamera() {
     'use strict';
-    camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
+
+    camera = new THREE.PerspectiveCamera( 70,
+                                          window.innerWidth / window.innerHeight,
+                                          1,
+                                          1000);
+                                
     camera.position.x = 50;
     camera.position.y = 50;
     camera.position.z = 50;
     camera.lookAt(scene.position);
+
 }
 
 function onResize() {
@@ -93,6 +90,18 @@ function onResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
     }
+
+}
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+    
+    scene.add(new THREE.AxisHelper(10));
+
+    createTable(0, 8, 0);
+    createBall(0, 0, 15)
 
 }
 
@@ -109,7 +118,7 @@ function onKeyDown(e) {
         });
         break;
     case 83:  //S
-    case 115: //s
+    case 115: //stabler
         ball.userData.jumping = !ball.userData.jumping;
         break;
     case 69:  //E
@@ -119,6 +128,18 @@ function onKeyDown(e) {
                 node.visible = !node.visible;
             }
         });
+        break;
+    case 37:
+        camera.position.x += 5;
+        break;
+    case 38:
+        camera.position.z += 5;
+        break;
+    case 39:
+        camera.position.x -= 5;
+        break;
+    case 40:
+        camera.position.z -= 5;
         break;
     }
 }
@@ -130,19 +151,22 @@ function render() {
 
 function init() {
     'use strict';
-    renderer = new THREE.WebGLRenderer({
-        antialias: true
-    });
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+
     renderer.setSize(window.innerWidth, window.innerHeight);
+
     document.body.appendChild(renderer.domElement);
 
     createScene();
+
     createCamera();
 
     render();
 
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("resize", onResize);
+
 }
 
 function animate() {
@@ -153,6 +177,7 @@ function animate() {
         ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
         ball.position.z = 15 * (Math.cos(ball.userData.step));
     }
+
     render();
 
     requestAnimationFrame(animate);
