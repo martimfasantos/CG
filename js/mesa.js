f2e5127dcf4f1d031344f479debf5c995f5132b7
martimfasantos <martimfasantos@gmail.com>
2022-05-12 10:35:58 +0100

    second class

--- js/mesa.js
+++ js/mesa.js
@@ -9,45 +9,10 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
 var ball;
-
-
-function createTable(x, y, z) {
-    'use strict';
-
-    var table = new THREE.Object3D();
-
-    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
-    
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
-    
-    scene.add(table);
-    
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
-}
-
-function addTableTop(obj, x, y, z) {
-    'use strict';
-
-    geometry = new THREE.CubeGeometry(60, 2, 20);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
-}
-
-function addTableLeg(obj, x, y, z) {
-    'use strict';
-
-    geometry = new THREE.CubeGeometry(2, 6, 2);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y - 3, z);
-    obj.add(mesh);
-}
+var clock = new THREE.Clock();
+var speed = 3; //units a second
+var delta = 0;
+var orthographicCamera;
 
 function createBall(x, y, z) {
     'use strict';
@@ -65,6 +30,55 @@ function createBall(x, y, z) {
     scene.add(ball);
 }
 
+function createBox(obj, x, y, z) {
+
+    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
+    box = new THREE.BoxGeometry(8, 8, 8);
+    mesh = new THREE.Mesh(box, material);
+
+    mesh.position.set(x, y, z);
+
+    obj.add(mesh);
+    
+}
+
+function createCylinder(obj, x, y, z) {
+
+    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
+    cylinder = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+    mesh = new THREE.Mesh(box, material);
+
+    mesh.position.set(x, y, z);
+
+    obj.add(mesh);
+    
+}
+
+function createIcosahedron(obj, x, y, z) {
+
+    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
+    icosahedron = new THREE.IcosahedronGeometry(5, 0);
+    mesh = new THREE.Mesh(icosahedron, material);
+
+    mesh.position.set(x, y, z);
+
+    obj.add(mesh);
+    
+}
+
+function createGeometry1(x, y, z){
+    var geometry1 = new THREE.Object3D();
+    
+    createBox(geometry1, 0, 0, 0);
+    createCylinder(geometry1, 0, 0, 8);
+    createIcosahedron(geometry1, 0, 0, 28);
+    
+    scene.add(geometry1);
+    
+    geometry1.position.x = x;
+    geometry1.position.y = y;
+    geometry1.position.z = z;
+}
 
 function createCamera() {
     'use strict';
@@ -79,6 +93,16 @@ function createCamera() {
     camera.position.z = 50;
     camera.lookAt(scene.position);
 
+}
+
+function createOrthographicCamera(x, y, z) {
+    'use strict';
+
+    orthographicCamera = THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 1, 1000);
+    camera.position.x = x;
+    camera.position.y = y;
+    camera.position.z = z;
+    camera.lookAt(scene.position);
 }
 
 function onResize() {
@@ -100,7 +124,7 @@ function createScene() {
     
     scene.add(new THREE.AxisHelper(10));
 
-    createTable(0, 8, 0);
+    createGeometry1(0, 8, 0);
     createBall(0, 0, 15)
 
 }
@@ -109,8 +133,23 @@ function onKeyDown(e) {
     'use strict';
 
     switch (e.keyCode) {
-    case 65: //A
-    case 97: //a
+    case 49: //1
+        // creat
+        camera.lookAt(scene.position);
+        break;
+    case 50: //2
+        camera.position.x = 0;
+        camera.position.y = 50;
+        camera.position.z = 0;
+        camera.lookAt(scene.position);
+        break;
+    case 51: //3
+        camera.position.x = 50;
+        camera.position.y = 5 ;
+        camera.position.z = 0;
+        camera.lookAt(scene.position);
+        break; 
+    case 52: //4
         scene.traverse(function (node) {
             if (node instanceof THREE.Mesh) {
                 node.material.wireframe = !node.material.wireframe;
@@ -162,8 +201,6 @@ function init() {
 
     createCamera();
 
-    render();
-
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("resize", onResize);
 
@@ -171,12 +208,16 @@ function init() {
 
 function animate() {
     'use strict';
-
+    
+    delta = clock.getDelta();
     if (ball.userData.jumping) {
-        ball.userData.step += 0.04;
+        ball.userData.step += 4 * delta;
         ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
         ball.position.z = 15 * (Math.cos(ball.userData.step));
     }
+
+    // object.position.y += speed * delta;
+    // object.position.z += speed * delta;
 
     render();
 
