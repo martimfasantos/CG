50ed2c68b063ebddc60039c01597d506941d3cfa
martimfasantos <marty.f.a.santos@gmail.com>
2022-05-12 18:26:17 +0100

    some figures added

--- js/main.js
+++ js/main.js
@@ -10,86 +10,199 @@ var camera, scene, renderer;
 var geometry, material, mesh;
 var ball;
 var clock = new THREE.Clock();
-var speed = 3; //units a second
 var delta = 0;
-var orthographicCamera;
 var cameras = [];
+var objs = [];
 
 function createBall(x, y, z) {
     'use strict';
 
     ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0 };
 
-    material = new THREE.MeshBasicMaterial({ color: 0x40E0D0, wireframe: true });
+    material = new THREE.MeshPhongMaterial({ color: 0x40E0D0, wireframe: true });
     geometry = new THREE.SphereGeometry(4, 10, 10);
     mesh = new THREE.Mesh(geometry, material);
 
-    ball.add(mesh);
     ball.position.set(x, y, z);
+    ball.add(mesh);
 
     scene.add(ball);
 }
 
-function createBox(obj, x, y, z) {
+function createBox(x, y, z, angle_x, angle_y, angle_z) {
 
-    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
-    box = new THREE.BoxGeometry(8, 8, 8);
-    mesh = new THREE.Mesh(box, material);
+    box = new THREE.Object3D();
+
+    material = new THREE.MeshPhongMaterial({ color: 0xff6600, wireframe: true });
+    geometry = new THREE.BoxGeometry(8, 8, 8, 2, 2);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = angle_x;
+    mesh.rotation.y = angle_y;
+    mesh.rotation.z = angle_z;
 
     mesh.position.set(x, y, z);
+    box.add(mesh);
 
-    obj.add(mesh);
+    scene.add(box);
     
 }
 
-function createCylinder(obj, x, y, z) {
+function createPanel(x, y, z, angle_x, angle_y, angle_z) {
 
-    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
-    cylinder = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-    mesh = new THREE.Mesh(box, material);
+    box = new THREE.Object3D();
+
+    material = new THREE.MeshPhongMaterial({ color: 0x437f5b, wireframe: true });
+    geometry = new THREE.BoxGeometry(1, 20, 25, 30, 20);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = angle_x;
+    mesh.rotation.y = angle_y;
+    mesh.rotation.z = angle_z;
 
     mesh.position.set(x, y, z);
+    box.add(mesh);
 
-    obj.add(mesh);
+    scene.add(box);
     
 }
 
-function createIcosahedron(obj, x, y, z) {
+function createLathe(x, y, z, angle_x, angle_y, angle_z){
 
-    material = new THREE.MeshBasicMaterial({ color: 0xB87333, wireframe: true });
-    icosahedron = new THREE.IcosahedronGeometry(5, 0);
-    mesh = new THREE.Mesh(icosahedron, material);
+    lathe = new THREE.Object3D();
+
+    const points = [];
+    for (let i = 0; i < 10; i ++) {
+        points.push( new THREE.Vector2(Math.sin(i * 0.2) * 10 + 5, (i - 5) * 2));
+    }
+    geometry = new THREE.LatheGeometry(points);
+    material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
+    lathe = new THREE.Mesh(geometry, material);
+    scene.add(lathe);
+}
+
+function createLine(x, y, z, angle_x, angle_y, angle_z) {
+
+    box = new THREE.Object3D();
+
+    material = new THREE.MeshPhongMaterial({ color: 0x437f5b, wireframe: true });
+    geometry = new THREE.BoxGeometry(1, 20, 25, 30, 20);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = angle_x;
+    mesh.rotation.y = angle_y;
+    mesh.rotation.z = angle_z;
 
     mesh.position.set(x, y, z);
+    box.add(mesh);
 
-    obj.add(mesh);
+    scene.add(box);
     
 }
 
-function createGeometry1(x, y, z){
-    var geometry1 = new THREE.Object3D();
+function createCylinder(x, y, z, angle_x, angle_y, angle_z) {
+
+    cylinder = new THREE.Object3D();
+
+    material = new THREE.MeshPhongMaterial({ color: 0xB87333, wireframe: true });
+    geometry = new THREE.CylinderGeometry(5, 5, 20, 35);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = angle_x;
+    mesh.rotation.y = angle_y;
+    mesh.rotation.z = angle_z;
+
+    mesh.position.set(x, y, z);
+    cylinder.add(mesh);
+
+    scene.add(cylinder);
     
-    createBox(geometry1, 0, 0, 0);
-    createCylinder(geometry1, 0, 0, 8);
-    createIcosahedron(geometry1, 0, 0, 28);
+}
+
+function createIcosahedron(x, y, z) {
+
+    icosahedron = new THREE.Object3D();
+
+    material = new THREE.MeshPhongMaterial({ color: 0xB1023D, wireframe: true });
+    geometry = new THREE.IcosahedronGeometry(5, 0);
+    mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.set(x, y, z);
+    icosahedron.add(mesh);
+
+    scene.add(icosahedron);
     
-    scene.add(geometry1);
+}
+
+function createExtrude(x, y, z, angle_x, angle_y, angle_z) {
+
+    extrude = new THREE.Object3D();
+
+    const length = 5, width = 3;
+
+    shape = new THREE.Shape();
+    shape.moveTo(0, 0);
+    shape.lineTo(0, width);
+    shape.lineTo(length, width);
+    shape.lineTo(length, 0);
+    shape.lineTo(0, 0);
+
+    const extrudeSettings = {
+        amount: 20,
+        steps: 5,
+        depth: 4,
+        bevelEnabled: true,
+        bevelThickness: 2,
+        bevelSize: 2,
+        bevelOffset: 4,
+        bevelSegments: 2
+
+    };
+
+    material = new THREE.MeshPhongMaterial({ color: 0xB123E2, wireframe: true });
+    geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = angle_x;
+    mesh.rotation.y = angle_y;
+    mesh.rotation.z = angle_z;
     
-    geometry1.position.x = x;
-    geometry1.position.y = y;
-    geometry1.position.z = z;
+    mesh.position.set(x, y, z);
+    extrude.add(mesh);
+
+    scene.add(extrude);
+    
+}
+
+function degreesToRadians(degrees){
+  return degrees * (Math.PI/180);
+}
+
+
+function createArticulatedObject(x, y, z){
+    // var geometry1 = new THREE.Object3D();
+    
+    // createBox(geometry1, 0, 0, 0);
+    // createCylinder(geometry1, 0, 0, 15, 0);
+    // createIcosahedron(geometry1, 0, 0, 28);
+    
+    // scene.add(geometry1);
+    
+    // geometry1.position.x = x;
+    // geometry1.position.y = y;
+    // geometry1.position.z = z;
 }
 
 function createCamera() {
     'use strict';
-    camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
+    camera = new THREE.PerspectiveCamera( 70,
+                                          window.innerWidth / window.innerHeight,
+                                          1,
+                                          1000 );
+    // Position
     camera.position.x = 50;
     camera.position.y = 50;
     camera.position.z = 50;
+
+    // point light
+    const light = new THREE.PointLight(0xffffff, 1);
+    camera.add(light);
+
     camera.lookAt(scene.position);
 
     return camera;
@@ -98,15 +211,21 @@ function createCamera() {
 function createOrthographicCamera(x, y, z) {
     'use strict';
 
-    orthographicCamera = new THREE.OrthographicCamera(window.innerWidth / - 20,
-                                                     window.innerWidth / 20,
-                                                     window.innerHeight / 20, 
-                                                     window.innerHeight / - 20, 
-                                                     1, 
-                                                     1000);
+    var orthographicCamera = new THREE.OrthographicCamera( window.innerWidth / - 20,
+                                                       window.innerWidth / 20,
+                                                       window.innerHeight / 20, 
+                                                       window.innerHeight / - 20, 
+                                                       1, 
+                                                       1000 );
+    // Position
     orthographicCamera.position.x = x;
     orthographicCamera.position.y = y;
     orthographicCamera.position.z = z;
+
+    // point light
+    const light = new THREE.PointLight(0xffffff, 1);
+    camera.add(light);
+    
     orthographicCamera.lookAt(scene.position);
 
     return orthographicCamera;
@@ -124,6 +243,18 @@ function onResize() {
 
 }
 
+
+function createObjects() {
+
+    createBall(0, 20, 0);
+    createBox(0, 10, 0, degreesToRadians(45), degreesToRadians(45), degreesToRadians(45));
+    createPanel(5, 20, -10, degreesToRadians(-45), degreesToRadians(-45), degreesToRadians(-45));
+    createCylinder(0, 0, 15, degreesToRadians(90), 0, 0);
+    createIcosahedron(10, -5, 14);
+    createExtrude(20, 5, -20, degreesToRadians(60), degreesToRadians(-30), 0)
+
+}
+
 function createScene() {
     'use strict';
 
@@ -131,8 +262,10 @@ function createScene() {
     
     scene.add(new THREE.AxisHelper(10));
 
-    createGeometry1(0, 8, 0);
-    createBall(0, 0, 15)
+    const light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+    scene.add( light );
+
+    createObjects();
 
 }
 
@@ -140,46 +273,45 @@ function onKeyDown(e) {
     'use strict';
 
     switch (e.keyCode) {
+        case 48: //0
+            camera = cameras[0]
+            break;
         case 49: //1
-        camera = cameras[1]
-        break;
-    case 50: //2
-        camera = cameras[2]
-        break;
-    case 51: //3
-        camera = cameras[3]
-        break; 
-    case 52: //4
-        scene.traverse(function (node) {
-            if (node instanceof THREE.Mesh) {
-                node.material.wireframe = !node.material.wireframe;
-            }
-        });
-        break;
-    case 83:  //S
-    case 115: //stabler
-        ball.userData.jumping = !ball.userData.jumping;
-        break;
-    case 69:  //E
-    case 101: //e
-        scene.traverse(function (node) {
-            if (node instanceof THREE.AxisHelper) {
-                node.visible = !node.visible;
-            }
-        });
-        break;
-    case 37:
-        camera.position.x += 5;
-        break;
-    case 38:
-        camera.position.z += 5;
-        break;
-    case 39:
-        camera.position.x -= 5;
-        break;
-    case 40:
-        camera.position.z -= 5;
-        break;
+            camera = cameras[1]
+            break;
+        case 50: //2
+            camera = cameras[2]
+            break;
+        case 51: //3
+            camera = cameras[3]
+            break; 
+        case 52: //4
+            scene.traverse(function (node) {
+                if (node instanceof THREE.Mesh) {
+                    node.material.wireframe = !node.material.wireframe;
+                }
+            });
+            break;
+        case 69:  //E
+        case 101: //e
+            scene.traverse(function (node) {
+                if (node instanceof THREE.AxisHelper) {
+                    node.visible = !node.visible;
+                }
+            });
+            break;
+        case 37:
+            camera.position.x += 5;
+            break;
+        case 38:
+            camera.position.z += 5;
+            break;
+        case 39:
+            camera.position.x -= 5;
+            break;
+        case 40:
+            camera.position.z -= 5;
+            break;
     }
 }
 
@@ -196,15 +328,32 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
-
+    
     createScene();
+    
+    // spotlights for the shadows
+    var spotLight1 = new THREE.SpotLight(0xffffff);
+    spotLight1.position.set(0, 30, 60);
+    spotLight1.castShadow = true;
+    spotLight1.intensity = 0.8;
+    scene.add(spotLight1);
 
+    var spotLight2 = new THREE.SpotLight(0xffffff);
+    spotLight2.position.set(10, -25, 0);
+    spotLight2.castShadow = true;
+    spotLight2.intensity = 1;
+    scene.add(spotLight2);
+
+
+    // cameras
     cameras.push(createCamera());
     cameras.push(createOrthographicCamera(0, 0, 50));
     cameras.push(createOrthographicCamera(0, 50, 0));
     cameras.push(createOrthographicCamera(50, 0, 0));
     camera = cameras[0];
 
+
+    // events
     window.addEventListener("keydown", onKeyDown);
     window.addEventListener("resize", onResize);
 
@@ -215,13 +364,10 @@ function animate() {
     
     delta = clock.getDelta();
     if (ball.userData.jumping) {
-        ball.userData.step += 4 * delta;
+        ball.userData.step += 3 * delta;
         ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
         ball.position.z = 15 * (Math.cos(ball.userData.step));
     }
-
-    // object.position.y += speed * delta;
-    // object.position.z += speed * delta;
 
     render();
 
