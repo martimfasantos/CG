dd18bb8d4793fe87ee063aca1d39679c96e1a5b3
martimfasantos <martimfasantos@gmail.com>
2022-05-17 14:33:19 +0100

    major changes

--- js/main.js
+++ js/main.js
@@ -9,60 +9,19 @@
 var camera, scene, renderer;
 var clock = new THREE.Clock();
 
-var speed = 1;
-const delta = 0;
+var speed = 40;
 const distance = 1;
-const angle = 0.05
+const angle = 0.05;
 var rotationLimit = 0;
 
 var cameras = [];
 var objs = [];
 var keyMap = [];
 
-var lamp;
-
-class ArticulatedObject {
-
-    constructor(base, middle, top){
-        this._base = base;
-        this._middle = middle;
-        this._top = top;
-    }
-
-    move_x(distance) {
-        this._base.position.x += distance;
-        this._middle.position.x += distance;
-        this._top.position.x += distance;
-    }
-
-    move_y(distance) {
-        this._base.position.y += distance;
-        this._middle.position.y += distance;
-        this._top.position.y += distance;
-    }
-
-    move_z(distance) {
-        this._base.position.z += distance;
-        this._middle.position.z += distance;
-        this._top.position.z += distance;
-    }
-
-    rotate_base_y(angle) { 
-        this._base.rotation.y += angle;
-        this._middle.rotation.y += angle;
-        this._top.rotation.y += angle;
-    }
-
-    rotate_middle_y(angle) {
-        this._middle.rotation.y += angle;
-        this._top.rotation.y += angle;
-    }
-
-    rotate_top_y(angle) {
-        this._top.rotation.y += angle;
-    }
-
-}
+var lamp = { base : null,
+             neck : null,
+             lampshade : null
+            };
 
 function createPrimitive(x, y, z, angle_x, angle_y, angle_z, color, geometry, side, texture){
 
@@ -93,20 +52,26 @@ function degreesToRadians(degrees){
 
 function createLamp(x, y, z, angle_x, angle_y, angle_z){
 
-    const base = createPrimitive(x, y, z, angle_x, angle_y, angle_z, 0xA89F7B,
-        new THREE.CylinderGeometry(3.5, 4, 1, 18), THREE.DoubleSide, null);
-    
-    var curve = new THREE.CatmullRomCurve3( [
+    const lampshade = createPrimitive( x, y + 16.9 , z + 7, angle_x + degreesToRadians(115), angle_y, angle_z, 0xF5F5F5,
+        new THREE.CylinderGeometry(4.5, 1, 6, 20, 20, openEnded = true), THREE.DoubleSide, null);
+
+    const curve = new THREE.CatmullRomCurve3( [
         new THREE.Vector3( 0, 18, 4.5 ),
         new THREE.Vector3( 0, 20, 0 ),
         new THREE.Vector3( 0, 0.5, 0 )]);
 
     const neck = createPrimitive(x, y, z, 0, 0, 0, 0xA8A8A8,
         new THREE.TubeGeometry(curve, 20, 1, 8, closed = false), THREE.DoubleSide, null);
-    const lampshade = createPrimitive( x, y + 16.9 , z + 7, angle_x + degreesToRadians(115), angle_y, angle_z, 0xF5F5F5,
-        new THREE.CylinderGeometry(4.5, 1, 6, 20, 20, openEnded = true), THREE.DoubleSide, null);
+    neck.add(lampshade);
 
-    lamp = new ArticulatedObject(base, neck, lampshade);
+    const base = createPrimitive(x, y, z, angle_x, angle_y, angle_z, 0xA89F7B,
+        new THREE.CylinderGeometry(3.5, 4, 1, 18), THREE.DoubleSide, null);
+    base.add(neck);    
+
+    // Create object Lamp
+    lamp.base = base;
+    lamp.neck = neck;
+    lamp.lampshade = lampshade;
 
 }
 
@@ -133,10 +98,10 @@ function createCamera() {
 function createOrthographicCamera(x, y, z) {
     'use strict';
 
-    var orthographicCamera = new THREE.OrthographicCamera( window.innerWidth / - 20,
-                                                       window.innerWidth / 20,
-                                                       window.innerHeight / 20, 
-                                                       window.innerHeight / - 20, 
+    var orthographicCamera = new THREE.OrthographicCamera( window.innerWidth / - 2,
+                                                       window.innerWidth / 2,
+                                                       window.innerHeight / 2, 
+                                                       window.innerHeight / -2, 
                                                        1, 
                                                        1000 );
     // Position
@@ -156,72 +121,27 @@ function createOrthographicCamera(x, y, z) {
 
 function onResize() {
     'use strict';
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    if (window.innerHeight > 0 && window.innerWidth > 0) {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    }
-
-}
-
-function createCamera() {
-    'use strict';
-    camera = new THREE.PerspectiveCamera( 70,
-                                          window.innerWidth / window.innerHeight,
-                                          1,
-                                          1000 );
-    // Position
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-
-    // point light
-    const light = new THREE.PointLight(0xffffff, 1);
-    camera.add(light);
-
-    camera.lookAt(scene.position);
-
-    return camera;
-}
-
-function createOrthographicCamera(x, y, z) {
-    'use strict';
-
-    var orthographicCamera = new THREE.OrthographicCamera( window.innerWidth / - 20,
-                                                       window.innerWidth / 20,
-                                                       window.innerHeight / 20, 
-                                                       window.innerHeight / - 20, 
-                                                       1, 
-                                                       1000 );
-    // Position
-    orthographicCamera.position.x = x;
-    orthographicCamera.position.y = y;
-    orthographicCamera.position.z = z;
-
-    // point light
-    const light = new THREE.PointLight(0xffffff, 1);
-    camera.add(light);
     
-    orthographicCamera.lookAt(scene.position);
-
-    return orthographicCamera;
-}
-
-
-function onResize() {
-    'use strict';
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    if (window.innerHeight > 0 && window.innerWidth > 0) {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+    if (camera == cameras[0]) {   
+        if (window.innerHeight > 0 && window.innerWidth > 0) {
+            
+            var aspect = window.innerWidth / window.innerHeight;
+            camera.left = -aspect * viewSize / 2;
+            camera.right = aspect * viewSize  / 2;
+            camera.top = viewSize / 2;
+            camera.bottom = -viewSize / 2;
+            
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        }
     }
 
-}
+    else {
+        
+    }
 
+
+}
 
 function createObjects() {
 
@@ -359,22 +279,21 @@ function onKeyDown(e) {
                 }
             });
             break;
-        case 171: //+
-        case 107: //Numpad+
-            if (speed < 2) {
-                speed += 0.05; 
+        case 77: //M
+        case 109: //m
+            if (speed < 300) {
+                speed += 10; 
                 console.log(speed);
             }
             break;
-        case 173: //-
-        case 109: //Numpad-
-            if (speed > 0.05) {
-                speed -= 0.05;
+        case 78: //N
+        case 110: //n
+            if (speed > 10) {
+                speed -= 10;
                 console.log(speed);
             }
             break;
         default:
-            movement();
             break;
     }
 }
@@ -383,59 +302,6 @@ function onKeyUp(e) {
     'use strict';
 
     keyMap[e.keyCode] = false;
-}
-
-function movement() {
-    'use strict';
-
-    const step = angle * speed;
-
-    // Translation
-    if (keyMap[38] == true) { //ArrowUp
-        lamp.move_y( distance );
-    }    
-    if (keyMap[40] == true) { //ArrowDown
-        lamp.move_y( -distance );
-    } 
-    if (keyMap[39] == true) { //ArrowRight
-        lamp.move_x( distance );
-    }
-    if (keyMap[37] == true) { //ArrowLeft
-        lamp.move_x( -distance );
-    }
-    if (keyMap[68] == true || keyMap[100] == true) { //D or d
-        lamp.move_z( distance );
-    }  
-    if (keyMap[67] == true || keyMap[99] == true) { //C or c
-        lamp.move_z( -distance );
-    }
-
-    // Rotation
-    if (keyMap[81] == true || keyMap[113] == true) { //Q or q
-        lamp.rotate_base_y( step );
-    }
-    if (keyMap[87] == true || keyMap[119] == true) { //W or w
-        lamp.rotate_base_y( -step );
-    }
-    if (keyMap[65] == true || keyMap[97] == true) { //A or a
-        lamp.rotate_middle_y( step );
-    }
-    if (keyMap[83] == true || keyMap[115] == true) { //S or s
-        lamp.rotate_middle_y( -step );
-    }
-    if (keyMap[90] == true || keyMap[122] == true) { //Z or z
-        if (rotationLimit <= 3*step) {
-            lamp.rotate_top_y( step );
-            rotationLimit += step;
-        }
-    }
-    if (keyMap[88] == true || keyMap[120] == true) { //X or x
-        if (rotationLimit >= -3*step) {
-            lamp.rotate_top_y( -step );
-            rotationLimit -= step
-        }
-    }
-
 }
 
 function render() {
@@ -486,12 +352,55 @@ function init() {
 function animate() {
     'use strict';
     
-    // delta = clock.getDelta();
-    // if (ball.userData.jumping) {
-    //     ball.userData.step += 3 * delta * speed;
-    //     ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
-    //     ball.position.z = 15 * (Math.cos(ball.userData.step));
-    // }
+    var delta = clock.getDelta();
+    const rotationStep = angle * speed * delta;
+    const translationStep = distance * speed * delta;
+
+    // Translation
+    if (keyMap[38] == true) { //ArrowUp
+        lamp.base.translateY( translationStep );
+    }    
+    if (keyMap[40] == true) { //ArrowDown
+        lamp.base.translateY( -translationStep );
+    } 
+    if (keyMap[39] == true) { //ArrowRight
+        lamp.base.translateX( translationStep );
+    }
+    if (keyMap[37] == true) { //ArrowLeft
+        lamp.base.translateX( -translationStep );
+    }
+    if (keyMap[68] == true || keyMap[100] == true) { //D or d
+        lamp.base.translateZ( translationStep );
+    }  
+    if (keyMap[67] == true || keyMap[99] == true) { //C or c
+        lamp.base.translateZ( -translationStep );
+    }
+
+    // Rotation
+    if (keyMap[81] == true || keyMap[113] == true) { //Q or q
+        lamp.base.rotateY( rotationStep );
+    }
+    if (keyMap[87] == true || keyMap[119] == true) { //W or w
+        lamp.base.rotateY( -rotationStep );
+    }
+    if (keyMap[65] == true || keyMap[97] == true) { //A or a
+        lamp.neck.rotateY( rotationStep );
+    }
+    if (keyMap[83] == true || keyMap[115] == true) { //S or s
+        lamp.neck.rotateY( -rotationStep );
+    }
+    if (keyMap[90] == true || keyMap[122] == true) { //Z or z
+        if (rotationLimit <= 3*rotationStep) {
+            lamp.lampshade.rotateY( rotationStep );
+            rotationLimit += rotationStep;
+        }
+    }
+    if (keyMap[88] == true || keyMap[120] == true) { //X or x
+        if (rotationLimit >= -3*rotationStep) {
+            lamp.lampshade.rotateY( -rotationStep );
+            rotationLimit -= rotationStep
+        }
+    }
 
     render();
 
