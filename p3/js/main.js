9f762f7abc51b825ba39bd61ecbe0c71ac218edd
martimfasantos <marty.f.a.santos@gmail.com>
2022-06-10 00:44:00 +0100

    spotligths

--- p3/js/main.js
+++ p3/js/main.js
@@ -9,6 +9,9 @@ const ARROWUP = 38;
 const ARROWDOWN = 40;
 const ARROWRIGHT = 39;
 const ARROWLEFT = 37;
+const HEIGHT = 12;
+const WIDTH = 20;
+const LENGTH = 100;
 
 // Translaction
 var speed = 5;
@@ -29,6 +32,7 @@ var cameras = [];
 var materials = [];
 var primitives = [];
 var keyPressed = [];
+var lights = [];
 
 // Objects
 var origami1, origami2, origami3;
@@ -71,7 +75,7 @@ function createCamera(x, y, z) {
     camera.position.y = y;
     camera.position.z = z;
 
-    camera.lookAt(scene.position);
+    camera.lookAt(0, 0, 0);
 
     cameras.push(camera);
 
@@ -138,17 +142,13 @@ function createObjects() {
     const textureLoader = new THREE.TextureLoader();
 
     // Displayer
-    const width = 12;
-    const length = 70;
-    const height = 10;
     createPrimitive(0, 0, 0, 0, 0, 0, null,
-        new THREE.BoxGeometry(2*width, height, length, 25), THREE.DoubleSide,
+        new THREE.BoxGeometry(1.5*WIDTH, HEIGHT, LENGTH, 25, 25), THREE.DoubleSide,
         textureLoader.load('../textures/wood.jpg'));
     
-    createPrimitive(-width, height/2, 0, 0, 0, 0, null,
-        new THREE.BoxGeometry(width, 2*height, length, 25), THREE.DoubleSide,
+    createPrimitive(-1.25*WIDTH, HEIGHT, 0, 0, 0, 0, null,
+        new THREE.BoxGeometry(WIDTH, 3*HEIGHT, LENGTH, 25, 25), THREE.DoubleSide,
         textureLoader.load('../textures/wood.jpg'));
-
 
     // --------------------------------
 
@@ -233,6 +233,38 @@ function render() {
     renderer.render(scene, camera);
 }
 
+function setupLights() {
+
+    // Directional Light
+    const dirLight = new THREE.DirectionalLight(0x404040, 0.4);
+    dirLight.position.set(5, HEIGHT, 0);
+    lights.push(dirLight);
+    scene.add(dirLight);
+
+    // Spotlights
+    const spotLight1 = new THREE.SpotLight(0xFFFFFF, 0.7);
+    spotLight1.position.set(0, 1.5*HEIGHT, -LENGTH/2 + LENGTH/8);
+    spotLight1.angle = Math.PI/8;
+    /* spotLight2.target(origami1); */
+    lights.push(spotLight1);
+    scene.add(spotLight1);
+
+    const spotLight2 = new THREE.SpotLight(0xFFFFFF, 0.7);
+    spotLight2.position.set(0, 1.5*HEIGHT, 0);
+    spotLight2.angle = Math.PI/8;
+    /* spotLight2.target(origami2); */
+    lights.push(spotLight2);
+    scene.add(spotLight2);
+
+    const spotLight3 = new THREE.SpotLight(0xFFFFFF, 0.7);
+    spotLight3.position.set(0, 1.5*HEIGHT, LENGTH/2 - LENGTH/8);
+    spotLight3.angle = Math.PI/8;
+    /* spotLight2.target(origami3); */
+    lights.push(spotLight3);
+    scene.add(spotLight3);
+
+}
+
 function init() {
     'use strict';
 
@@ -245,18 +277,16 @@ function init() {
     
     createScene();
     
-    // Lights
-    const light1 = new THREE.DirectionalLight(0x404040, 5);
-    scene.add(light1);
+    setupLights();
 
-    const light2 = new THREE.AmbientLight(0xF7F7F7, 1);
-    scene.add(light2);
+    const ambLight = new THREE.AmbientLight(0xF7F7F7, 0.8);
+    scene.add(ambLight);
 
     // Cameras
     orthographicCamera = createOrthographicCamera(0, 0, cameraDist);
     scene.add(orthographicCamera);
 
-    perspectiveCamera = createCamera(cameraDist, cameraDist/1.2, cameraDist);
+    perspectiveCamera = createCamera(2*cameraDist, cameraDist/2, 0);
     scene.add(perspectiveCamera);
 
     camera = perspectiveCamera;
